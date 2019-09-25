0bd2ab9c74ab5f1774152c48457339ef2269e3e8
Manik Mehta <manik.vipandeep.mehta@uni-weimar.de>
2019-09-25 14:36:56 +0530

    Completed Prime & Doublette exercise.

--- Prime.java
+++ Prime.java
@@ -10,6 +10,27 @@ public class Prime {
 	//The function should return the smaller one of the doublette. If there exists no
 	//prime doublette between the given parameter and the maximal int-value (Integer.MAX_VALUE)
 	//the function should return 0.
-		
-	
+	public static void main(String[] args){
+		System.out.println(isPrime(100));
+		System.out.println(findDoublette(10));
+
+	}
+	public static boolean isPrime(int num){
+	    for(int i=2;i<num;i++){
+	    	if(num%i==0){
+	    		return false;
+	    	}
+	    }
+		return true;
+	}	
+	public static int findDoublette(int num){
+	    for(int i=num;i<Integer.MAX_VALUE;i++){
+	    	if(isPrime(i)){
+	    		if(isPrime(i+2)){
+	    			return i;
+	    		}
+	    	}
+	    }
+	    return 0;		
+	}
 }
